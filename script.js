37c9f82cb6cdea02c61017f65411a6b6632c6308
acemaster-gh <acewithmefrom0@gmail.com>
2026-05-13 23:00:36 +0530

    style: css created

--- script.js
+++ script.js
@@ -13,23 +13,34 @@ const DOM = {
    passwordError:           document.querySelector('#password-error'), 
    confirmPasswordError:    document.querySelector('#confirm-password-error'), 
    passwordCharacterMessage:document.querySelector('#password-character-message'),
+   successMessage:
+document.querySelector('#success-message'),
+
 };
+
+
 
 // 2. FUNCTIONS
 
 function validateUsername() {
     if (DOM.username.value.trim().length < 2) {
         DOM.usernameError.textContent = 'Name must be at least 2 characters';
+        DOM.username.classList.add('invalid');
+        DOM.username.classList.remove('valid');
+
+
         return false;
     }
     DOM.usernameError.textContent = '';
+    DOM.username.classList.add('valid');
+    DOM.username.classList.remove('invalid');
     return true;
 
 }
  
 function validateEmail() {
     const valid =  /^[^\s@]+@[^\s@]+\.[^\s@]+$/.test(DOM.email.value);
-    if (!value) {
+    if (!valid) {
         DOM.emailError.textContent = 'Enter a valid email address';
         return false;
     }
@@ -45,7 +56,7 @@ function validatePassword() {
         return false;
     }
 
-DOM.emailError.textContent = '';
+DOM.passwordError.textContent = '';
 return true;
 }
 
@@ -57,8 +68,9 @@ function validateConfirmPassword() {
 
     }
     DOM.confirmPasswordError.textContent = '';
-    return false;
+    return true;
 }
+
 
 // 3. EVENT LISTENERS
 
@@ -66,13 +78,33 @@ DOM.username.addEventListener('input', validateUsername);
 DOM.email.addEventListener('input', validateEmail);
 DOM.password.addEventListener('input', validatePassword);
 DOM.confirmPassword.addEventListener('input', validateConfirmPassword);
-
 DOM.form.addEventListener('submit', (e) => {
   e.preventDefault();
   
-  const valid = validateUsername() & validateEmail() & validatePassword() & validateConfirmPassword();
+  const valid = validateUsername() && validateEmail() && validatePassword() && validateConfirmPassword();
   
   if (valid) {
-    alert('Account created!');
+   DOM.successMessage.textContent =
+            '✅ Account created successfully!';
+              DOM.form.reset();
+
+      
+        const inputs = [
+            DOM.username,
+            DOM.email,
+            DOM.password,
+            DOM.confirmPassword
+        ];
+
+        inputs.forEach((input) => {
+            input.classList.remove('valid');
+            input.classList.remove('invalid');
+        });
+
+      
+        DOM.usernameError.textContent = '';
+        DOM.emailError.textContent = '';
+        DOM.passwordError.textContent = '';
+        DOM.confirmPasswordError.textContent = '';
   }
 });
